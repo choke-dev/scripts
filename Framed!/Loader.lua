54280f57bdc7a21fa928f467a749b8409aa7e2c6
choke <47597850+choke-dev@users.noreply.github.com>
2022-06-20 17:00:39 +0800

    fix the proximity prompt and added undercover esp

--- Framed!/Loader.lua
+++ Framed!/Loader.lua
@@ -2,5 +2,5 @@ getgenv().FramedTESP_Notifications = true
 game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt)
     fireproximityprompt(prompt)
 end)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/scripts/main/Framed!/Target%20ESP.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/scripts/main/Framed!/Target%20ESP.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Dependencies/skeleton%20esp.lua"))()
