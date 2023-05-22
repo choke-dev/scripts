180f283ec7e005be9668aed1989bf283e43242b4
choke <47597850+choke-dev@users.noreply.github.com>
2023-05-22 14:56:50 +0800

    Update Events.lua

--- Blockate/RoundSystem/Events.lua
+++ Blockate/RoundSystem/Events.lua
@@ -20,11 +20,25 @@ local function runCommand(text)
 end
 
 
+--=[ Url Encoder ]=--
+local char_to_hex = function(c)
+    return string.format("%%%02X", string.byte(c))
+end
+  
+local function urlencode(url)
+  if url == nil then
+    return
+  end
+  url = url:gsub("\n", "\r\n")
+  url = url:gsub("([^%w ])", char_to_hex)
+  url = url:gsub(" ", "+")
+  return url
+end
+--=[ End Url Encode ]=--
+
 local function shout(message)
     -- url encode the whole message string
-    message = message:gsub("([^%w ])", function(c)
-        return ("%%%02X"):format(string.byte(c))
-    end)
+    message = urlencode(message)
     pcall(function()
         game:GetService("ReplicatedStorage").Sockets.Command:InvokeServer("!shout "..message)
     end)
