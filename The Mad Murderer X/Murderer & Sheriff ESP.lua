778d65792308758aab2bfe77bc4b6deead987362
choke <47597850+choke-dev@users.noreply.github.com>
2023-05-26 11:14:39 +0800

    Update Murderer & Sheriff ESP.lua

--- The Mad Murderer X/Murderer & Sheriff ESP.lua
+++ The Mad Murderer X/Murderer & Sheriff ESP.lua
@@ -2,7 +2,7 @@
     // Name: [The Mad Murderer X] Sheriff / Murderer ESP
     // Author: choke#3588 <@208876506146013185>
     // Desc: "wish there was something else i can use"
-    // Date: 2023/05/25 [YYYY/MM/DD]
+    // Date: 2023/05/26 [YYYY/MM/DD]
 ]]
 
 local Services  = setmetatable({}, {
@@ -15,6 +15,16 @@ local Players = Services.Players
 
 function DroppedGun_Handler()
     -- workspace.Entities.DroppedGun.Handle
+    local DroppedGun = workspace.Entities:FindFirstChild("DroppedGun")
+
+    if not DroppedGun then return end
+
+    local DroppedGun_Highlight_Instance            = Instance.new("Highlight")
+    DroppedGun_Highlight_Instance.Parent           = DroppedGun
+    DroppedGun_Highlight_Instance.Name             = "DroppedGun_Highlight"
+    DroppedGun_Highlight_Instance.FillTransparency = 1
+    DroppedGun_Highlight_Instance.OutlineColor     = Color3.new(0, 1, 0)
+    return DroppedGun_Highlight_Instance
 end
 
 function AddESPHook(Character)
@@ -29,7 +39,6 @@ function AddESPHook(Character)
         local Sheriff_Highlight  = Character:FindFirstChild("Sheriff_Highlight")
 
         if not Murderer_Highlight and Knife then
-            print("Adding Highlight to Murderer: "..Character.Name)
             local Murderer_Highlight_Instance            = Instance.new("Highlight")
             Murderer_Highlight_Instance.Parent           = Character
             Murderer_Highlight_Instance.Name             = "Murderer_Highlight"
@@ -38,52 +47,41 @@ function AddESPHook(Character)
         end
 
         if not Sheriff_Highlight and Gun then
-            print("Adding Highlight to Sheriff: "..Character.Name)
             local Sheriff_Highlight_Instance            = Instance.new("Highlight")
             Sheriff_Highlight_Instance.Parent           = Character
             Sheriff_Highlight_Instance.Name             = "Sheriff_Highlight"
             Sheriff_Highlight_Instance.FillTransparency = 1
             Sheriff_Highlight_Instance.OutlineColor     = Color3.new(0, 0, 1)
+            DroppedGun_Handler()
         end
     end)
     return ESPHook
 end 
 
-function AddAliveHook(Character)
-    local AliveHook
-    AliveHook = Character.ChildAdded:Connect(function(Child)
-        if not Child:IsA("Folder") then return end
-        if Child.Name ~= "NoCollisionConstraints" then return end
 
-        local Murderer_Highlight = Character:FindFirstChild("Murderer_Highlight")
-        local Sheriff_Highlight  = Character:FindFirstChild("Sheriff_Highlight")
+workspace.DebrisClient.PersistingDeathEffects.ChildAdded:Connect(function(Child)
+    local Murderer_Highlight = Child:FindFirstChild("Murderer_Highlight")
+    local Sheriff_Highlight  = Child:FindFirstChild("Sheriff_Highlight")
 
-        if Murderer_Highlight then
-            Murderer_Highlight:Destroy()
-        end
-        
-        if Sheriff_Highlight then
-            Sheriff_Highlight:Destroy()
-        end
+    if Murderer_Highlight then
+        Murderer_Highlight:Destroy()
+    end
 
-    end)
-    return AliveHook
-end
-
+    if Sheriff_Highlight then
+        Sheriff_Highlight:Destroy()
+    end
+end)
 
 
 Players.PlayerAdded:Connect(function(Player)
     Player.CharacterAdded:Connect(function(Character)
         AddESPHook(Character)
-        AddAliveHook(Character)
     end)
 end)
 
-for _,Player in pairs(Players:GetChildren()) do
+for _, Player in pairs(Players:GetChildren()) do
     Player.CharacterAdded:Connect(function(Character)
         AddESPHook(Character)
-        AddAliveHook(Character)
     end)
     AddESPHook(Player.Character)
-    AddAliveHook(Player.Character)
 end
