bd820e348633e97a46e64080995abb4dd93d8859
choke <47597850+choke-dev@users.noreply.github.com>
2023-05-31 15:16:02 +0800

    Update Main.lua
    add a permission system

--- Blockate/BlockBot/Main.lua
+++ Blockate/BlockBot/Main.lua
@@ -10,49 +10,71 @@ getgenv().BlockateBot_Settings = {
     Commands_FilePath = "./Blockate/Bot/Commands.lua",
     Commands_Prefix = ".",
     
-    Whitelisted_Users = {
-        Services.Players.LocalPlayer.UserId
+    PermissionLevels = {
+        [Services.Players.LocalPlayer.UserId] = 5
     }
 }
 
-local CommandsTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/scripts/main/Blockate/BlockBot/Commands.lua"))()
+--=[ ! DO NOT EDIT ANYTHING BELOW THIS LINE ! ]=--
 
---[[
-if isfile(getgenv().BlockateBot_Settings.Commands_FilePath) then
-    CommandsTable = loadstring(readfile(getgenv().BlockateBot_Settings.Commands_FilePath))()
-else
-    writefile(getgenv().BlockateBot_Settings.Commands_FilePath, game:HttpGet("foo.lua"))
-    CommandsTable = loadstring(readfile(getgenv().BlockateBot_Settings.Commands_FilePath))()
-end
-]]
+getgenv().BlockateBot_Internal = {
+    -- CommandsTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/scripts/main/Blockate/BlockBot/Commands.lua"))(),
+    CommandsTable = loadstring(readfile(getgenv().BlockateBot_Settings.Commands_FilePath))(),
+    Connections = {},
+}
 
 --=[ Functions ]=--
-function commandHandler(user, message)
-    local Arguments = message:split(" ")
-    local Command = Arguments[1]:sub(#getgenv().BlockateBot_Settings.Commands_Prefix + 1)
-    table.remove(Arguments, 1)
+function sayMessage(text, includeBotName, user)
+    assert(type(text) == "string", "text must be a string")
 
-    if CommandsTable[Command] then
-        CommandsTable[Command](user, Arguments)
+    if includeBotName then
+        text = "[ " .. getgenv().BlockateBot_Settings.Bot_Name .. " ] " .. text
     end
+
+    if user then
+        Services.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(text, "To " .. user.Name)
+    else
+        Services.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(text, "All")
+    end
+end
+
+function commandHandler(user, message)
+    local commandName = message:sub(#getgenv().BlockateBot_Settings.Commands_Prefix + 1, #message):split(" ")[1]
+    local commandArgs = message:sub(#getgenv().BlockateBot_Settings.Commands_Prefix + #commandName + 2, #message):split(" ")
+
+    local command = getgenv().BlockateBot_Internal.CommandsTable[commandName]
+    local permissionLevel = getgenv().BlockateBot_Settings.PermissionLevels[user.UserId]
+
+    if not command then return end
+    
+    if permissionLevel < command["Permission"] then
+        --sayMessage("You do not have permission to use this command.", true)
+        return
+    end
+
+    command["Function"](user, commandArgs)
 end
 
 --=[ Internal Functions ]=--
 function HookPlayerChat(Player)
-    Player.Chatted:Connect(function(Message)
+    if Player ~= Services.Players.LocalPlayer then
+        getgenv().BlockateBot_Settings.PermissionLevels[Player.UserId] = 1
+    end
+    local PlayerChatted_Connection = Player.Chatted:Connect(function(Message)
         if Message:sub(1, #getgenv().BlockateBot_Settings.Commands_Prefix) ~= getgenv().BlockateBot_Settings.Commands_Prefix then return end
-        if not table.find(getgenv().BlockateBot_Settings.Whitelisted_Users, Player.UserId) then return end
 
         commandHandler(Player, Message)
     end)
+    table.insert(getgenv().BlockateBot_Internal.Connections, PlayerChatted_Connection)
 end
 
 --=[ Hooks ]=--
-Services.Players.PlayerAdded:Connect(function(Player)
+local PlayerAdded_Hook = Services.Players.PlayerAdded:Connect(function(Player)
     HookPlayerChat(Player)
-end)	
+end)
 
 for _, Player in pairs(Services.Players:GetPlayers()) do
     HookPlayerChat(Player)
 end
 
+table.insert(getgenv().BlockateBot_Internal.Connections, PlayerAdded_Hook)
