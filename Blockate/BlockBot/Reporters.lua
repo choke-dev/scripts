bf79324a1ff0cc2a3b99ee49a38f86ac20421d7c
choke <47597850+choke-dev@users.noreply.github.com>
2023-06-05 20:40:19 +0800

    Update Reporters.lua
    FIX Syntax error

--- Blockate/BlockBot/Reporters.lua
+++ Blockate/BlockBot/Reporters.lua
@@ -1,7 +1,7 @@
 -- This file contains every roblox userid that has reported an instance of BlockBot to #blockate-reports.
 return {
-  159671173 = {
-    Timestamp = "1685909940"
+  [159671173] = {
+    Timestamp = "1685909940",
     Evidence = {
       "https://user-images.githubusercontent.com/47597850/243248523-ba62d2e8-e0a1-45c5-b7a9-a9fd9dc455a5.png"
     }
