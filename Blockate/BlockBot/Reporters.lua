789a05c0da757fcf31fd965d6d0862911fa58249
choke <47597850+choke-dev@users.noreply.github.com>
2023-06-07 02:19:24 +0800

    Update Reporters.lua

--- Blockate/BlockBot/Reporters.lua
+++ Blockate/BlockBot/Reporters.lua
@@ -1,10 +1,21 @@
 -- This file contains every roblox userid that has reported an instance of BlockBot to #blockate-reports.
 return {
   [159671173] = {
-    Timestamp = "1685909940",
+    IDs = {
+      Discord = 453182854088753155
+    },
+    Timestamp = 1685909940,
     Evidence = {
       "https://user-images.githubusercontent.com/47597850/243248523-ba62d2e8-e0a1-45c5-b7a9-a9fd9dc455a5.png"
     }
   },
-  [1044323098] = "TEMPORARY"
+  [1408877525] = {
+    IDs = {
+      Discord = 494489804759760899
+    },
+    Timestamp = 1686019140,
+    Evidence = {
+      "https://user-images.githubusercontent.com/47597850/243248523-ba62d2e8-e0a1-45c5-b7a9-a9fd9dc455a5.png"
+    }
+  },
 }
